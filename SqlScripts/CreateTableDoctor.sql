651fa6847394299674df710d6c4673ea73a564d8
Ashlesh-Devdare <adevdare@us.ibm.com>
2019-11-26 18:48:44 +0530

    Adding DB Scripts

---
+++ SqlScripts/CreateTableDoctor.sql
@@ -0,0 +1,6 @@
+CREATE TABLE doctors (
+first_name varchar(25),
+last_name  varchar(25),
+department varchar(15),
+email  varchar(50)
+);
---
+++ SqlScripts/InsertData.sql
@@ -0,0 +1,22 @@
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('Lorenz', 'Vanthillo', 'OPD', 'lvthillo@mail.com');
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('Natasha', 'Parker', 'OPD', 'nparker@mail.com');
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('Luis', 'Austin', 'Pathology', 'laustin@mail.com');
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('John', 'Smith', 'OPD', 'jsmith@mail.com');
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('Akshay', 'Sharma', 'OPD', 'rsharma@mail.com');
+INSERT INTO doctors (first_name, last_name, department, email) 
+VALUES ('Puja', 'Joshi', 'OPD', 'pjoshi@mail.com');
+INSERT INTO patients (first_name, last_name) 
+VALUES ('Lorenz', 'Vanthillo');
+INSERT INTO patients (first_name, last_name) 
+VALUES ('Steve', 'Lee');
+INSERT INTO patients (first_name, last_name) 
+VALUES ('Peter', 'Stanly');
+INSERT INTO patients (first_name, last_name) 
+VALUES ('Tim', 'Smith');
+INSERT INTO patients (first_name, last_name) 
+VALUES ('Bruce', 'Stark');
